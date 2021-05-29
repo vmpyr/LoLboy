0ccda7c9c3640b7d03fe6edaa43081ab53eee302
vmpyr <sarswat_ur@me.iitr.ac.in>
2021-05-29 18:15:53 +0530

    learnt and tried some more stuff

--- src/bot.js
+++ src/bot.js
@@ -2,13 +2,23 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const PREFIX = "-";
 
 client.on('ready', () => {
     console.log(`${client.user.username} just logged in!`);
 });
 
 client.on('message', (message) => {
-    console.log(`${message.author.username} sent ${message.content}`);
+    if (message.author.bot) return;
+    
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+        console.log(CMD_NAME);
+        console.log(args);  
+    }
 });
 
 client.login(process.env.LOLBOY_TOKEN);
